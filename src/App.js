fc174691c7a538d863fedd9582c86338a2a1ebd9
Franco Panvini <francopanvini@gmail.com>
2021-07-29 15:33:44 -0300

    Update fetchCity in single file

--- src/App.js
+++ src/App.js
@@ -1,45 +1,22 @@
 import React, { useState } from "react";
+
 import ImageBackgroud from "./components/ImageBackground.jsx";
 import Content, { ContentRow, ContentFlexBox } from "./components/Content";
 import MainCard from "./components/MainCard";
 import Cards from "./components/Cards.jsx";
 import SearchBar from "./components/SearchBar";
+  
+import fetchCity from './services/fetchCity'
 
 import image from "./media/background2.jpg";
 
 function App() {
   const [cities, setCities] = useState([]);
-
-  const apiKey = process.env.REACT_APP_API_KEY;
+  
   function onSearch(ciudad) {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`
-    )
-      .then((r) => r.json())
-      .then((recurso) => {
-        if (recurso.main !== undefined) {
-          const ciudad = {
-            id: recurso.id,
-            name: recurso.name,
-            weather: recurso.weather[0].main,
-            img: recurso.weather[0].icon,
-            temp: recurso.main.temp,
-            termica: recurso.main.feels_like,
-            min: Math.round(recurso.main.temp_min),
-            max: Math.round(recurso.main.temp_max),
-            presion: recurso.main.pressure,
-            humidity: recurso.main.humidity,
-            wind: recurso.wind.speed,
-            clouds: recurso.clouds.all,
-            latitud: recurso.coord.lat,
-            longitud: recurso.coord.lon,
-          };
-          setCities((oldCities) => [...oldCities, ciudad]);
-        } else {
-          alert("Ciudad no encontrada");
-        }
-      });
+    return fetchCity (ciudad, setCities);
   }
+
 
   function onClose(id) {
     setCities((oldCities) => oldCities.filter((c) => c.id !== id));
---
+++ src/services/fetchCity.js
@@ -0,0 +1,33 @@
+const apiKey = process.env.REACT_APP_API_KEY;
+
+function fetchCity(ciudad, setCities) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`
+  )
+    .then((r) => r.json())
+    .then((recurso) => {
+      if (recurso.main !== undefined) {
+        const ciudad = {
+          id: recurso.id,
+          name: recurso.name,
+          weather: recurso.weather[0].main,
+          img: recurso.weather[0].icon,
+          temp: recurso.main.temp,
+          termica: recurso.main.feels_like,
+          min: Math.round(recurso.main.temp_min),
+          max: Math.round(recurso.main.temp_max),
+          presion: recurso.main.pressure,
+          humidity: recurso.main.humidity,
+          wind: recurso.wind.speed,
+          clouds: recurso.clouds.all,
+          latitud: recurso.coord.lat,
+          longitud: recurso.coord.lon,
+        };
+        setCities((oldCities) => [...oldCities, ciudad]);
+      } else {
+        alert("Ciudad no encontrada");
+      }
+    });
+}
+
+export default fetchCity
